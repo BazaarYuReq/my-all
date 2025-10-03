34b0d358c02c251c35b51f0254f54b8dc9e29822
PADA-1-3-16 <pada-1-3-16@BEGUS.local>
2025-10-03 18:38:03 +0800

    sdfds

---
+++ hicheel/2.js
@@ -0,0 +1,36 @@
+let age = 16;
+if (age < 18) {
+  console.log("not adult");
+} else if (age >= 18) {
+  console.log("adult");
+}
+
+let number = 11;
+if (number % 2 === 0) {
+  console.log("tegsh");
+} else if (number % 2 === 1) {
+  console.log("sondgoi");
+}
+
+let a = 30;
+let b = 20;
+
+if (a > b) {
+  console.log("a is higher than b");
+} else if (a === b) {
+  console.log("a is equal to b");
+} else {
+  console.log("a is lower than b");
+}
+
+let score = 60;
+
+if (score >= 90) {
+  console.log("A");
+} else if (score >= 80) {
+  console.log("B");
+} else if (score >= 70) {
+  console.log("C");
+} else {
+  console.log("F");
+}
---
+++ hicheel/index.js
@@ -0,0 +1,17 @@
+let a = 1;
+let b = 2;
+let c = 3;
+
+if (a > b) {
+  if (a > c) {
+    console.log(a);
+  } else {
+    console.log(c);
+  }
+} else {
+  if (b > c) {
+    console.log(b);
+  } else {
+    console.log(c);
+  }
+}
